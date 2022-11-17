68e4b79d3be27826176ef0b5fa1619427478200e
ljllanosl <ljllanosl91@gmail.com>
2022-11-16 22:27:41 -0500

    created show page components and populated with data

---
+++ src/components/show/Details.jsx
@@ -0,0 +1,14 @@
+const Details = ({ status, premiered, network }) => {
+  return (
+    <div>
+      <p>
+        Status: <span>{status}</span>
+      </p>
+      <p>
+        Premiered {premiered} {network ? `on ${network.name}` : null}
+      </p>
+    </div>
+  );
+};
+
+export default Details;
